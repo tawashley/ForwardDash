2cce5f414bb40c1f37abfb65612aa00b96520f5d
tawashley <trisashley@gmail.com>
2014-08-29 13:29:25 +0100

    feat(Widget): Initial commit for current weather widget

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     var Manager = WidgetManager();
 
-    //@TODO figure out have to pass config from registerWidget into the widget object;
     Manager.registerWidget({
         name: 'ClockWidget',
         size: 'small',
@@ -11,6 +10,12 @@ document.addEventListener("DOMContentLoaded", function(){
             // showDate: false,
            // TwelvehourClock: true
         }
+    })
+
+    Manager.registerWidget({
+        name: 'CurrentWeatherWidget',
+        size: 'medium',
+        position: 'right'
     })
 
     Manager.renderWidgets();
---
+++ js/widgets/CurrentWeatherWidget/CurrentWeatherWidget.js
@@ -0,0 +1,18 @@
+function CurrentWeatherWidget() {
+
+    //maybe look at having a wrapper function e.g. setConifg()
+    //wrap around 'undefined' check
+
+    // var _config = {
+    //     showDate: ((config.showDate !== undefined) ? config.showDate : true),
+    //     showSeconds: ((config.showSeconds !== undefined) ? config.showSeconds : true),
+    //     showTimeOfDayIcon: ((config.showTimeOfDayIcon !== undefined) ? config.showTimeOfDayIcon : true),
+    //     TwelvehourClock: ((config.TwelvehourClock !== undefined) ? config.TwelvehourClock : false)
+    // };
+
+    exports.init = function() {
+        console.log('hits current weather widget init')
+    };
+
+    return exports;
+};
