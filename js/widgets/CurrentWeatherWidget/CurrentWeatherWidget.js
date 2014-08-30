82380fb0f68abc6a8ef6abc7c9256e8d2b5cb936
tawashley <trisashley@gmail.com>
2014-08-30 10:55:56 +0100

    feat(WeatherWidget): Get weather data and render temp, min, max on UI

--- js/widgets/CurrentWeatherWidget/CurrentWeatherWidget.js
+++ js/widgets/CurrentWeatherWidget/CurrentWeatherWidget.js
@@ -1,11 +1,14 @@
 function CurrentWeatherWidget(config, helpers) {
 
-    //maybe look at having a wrapper function e.g. setConifg()
-    //wrap around 'undefined' check
-
     // var _config = {
     //     showDate: ((config.showDate !== undefined) ? config.showDate : true),
     // };
+
+    var _position;
+
+    var _dom = {
+        widgetContainer: document.getElementById('current-weather-container')
+    }
 
     function _getUserLocation() {
         helpers.getLocation({
@@ -15,24 +18,44 @@ function CurrentWeatherWidget(config, helpers) {
                 maximumAge : 0 // spec default is 0
             },
             completeCallback: function(position) {
-                _weatherAPIRequest(position);
+                _position = position;
+                _weatherAPIRequest();
             }
         })
     }
 
-    function _weatherAPIRequest(position) {
+    function _weatherAPIRequest() {
         helpers.asyncRequest({
             method: 'GET',
             type: 'json',
-            uri: 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude
+            //metric = degrees c
+            //imperial = fahrenheight
+            uri: 'http://api.openweathermap.org/data/2.5/weather?lat=' + _position.coords.latitude + '&lon=' + _position.coords.longitude + '&units=metric'
         }, function(response){
-            console.log(response);
+            _renderWeatherUI(response);
         });
+    }
+
+    function _renderWeatherUI(response){
+        console.log(response);
+
+        var html = [];
+
+        html.push('<div class="weather-data max-temp">Current Temperature: ' + response.main.temp_max.toFixed(0) + '</div>');
+        html.push('<div class="weather-data max-temp">Max Temp: ' + response.main.temp_max.toFixed(0) + '</div>');
+        html.push('<div class="weather-data min-temp">Min Temp: ' + response.main.temp_min.toFixed(0) + '</div>');
+
+        _dom.widgetContainer.innerHTML = html.join('');
+    }
+
+    function _loadingUIMessage(){
+        _dom.widgetContainer.innerHTML = '<p>Getting weather data...</p>';
     }
 
     var exports = {};
 
     exports.init = function() {
+        _loadingUIMessage();
         _getUserLocation();
     };
 
